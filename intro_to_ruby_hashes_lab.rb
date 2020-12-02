3abfd30afebcf6a125ee47d2346449c8b9ae50fb
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 04:30:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,25 @@ def monopoly_with_third_tier
 	  :railroads =>
 	  {
 	    :pieces => 4,
-	    :names=> {},
+	    :names=>     
+	    {
+        :reading_railroad=>
+        {
+          "mortgage_value"=>"$100"
+        },
+        :pennsylvania_railroad=>
+        {
+          "mortgage_value"=>"$200"
+        },
+        :b_and_o_railroad=>
+        {
+          "mortgage_value"=>"$400"
+        },
+        :shortline=>
+        {
+          "mortgage_value"=>"$800"
+        }
+,
 	    :rent_in_dollars=> 
 	    {
 	      :one_piece_owned=>25,
